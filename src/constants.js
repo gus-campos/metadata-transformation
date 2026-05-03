5d57b9bfc279d26752e1961d031183b86d5ecc56
Gustavo Campos <gus.campos042@gmail.com>
2026-05-02 22:18:28 -0300

    adjust constants

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,17 @@
-export const metaPropsKeys = ["readonly", "required", "hidden", "breakLine", "size"];
+export const metaPropsKeys = [
+  "readonly",
+  "required",
+  "hidden",
+  "breakLine",
+  "size",
+];
+
 export const booleanMetaProps = ["readonly", "required", "hidden", "breakLine"];
+
 export const sizeValidValues = ["sm", "md", "lg"];
+
 export const exclusiveKeys = ["_field", "_fields", "_if"];
+
 export const dependantToExclusiveKeys = {
   _field: ["_is", "_isNot", "_isIn"],
   _fields: ["_are", "_someIs"],
